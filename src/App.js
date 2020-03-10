522c41dba34c7e0bf01f34e4cf26f008495cbc9b
sangsooleea <dulsang@naver.com>
2020-03-11 02:19:54 +0900

    기능, 컴포넌트별 분할 0.2 Router 구축

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+import {Board, Main} from "./index";
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Route exact path="/" component={Main}/>
+                <Route exact path="/board" component={Board}/>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
+export {default as Board} from './component/board';
+export {default as Main} from './component/main'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
